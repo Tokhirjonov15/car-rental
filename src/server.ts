4e6ca075e4eb861dbf463ff48266f5d539596f1a
Alex <mntokhirjonov@gmail.com>
2025-12-02 11:24:22 +0500

    feat: connect MongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,26 @@
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
+mongoose.set('strictQuery', false);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log('MongoDB Connected');
+    const PORT = process.env.PORT ?? 3006;
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB error:', err);
+});
+
+connectDB();
