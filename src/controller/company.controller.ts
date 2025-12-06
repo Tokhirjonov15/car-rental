6e61a2db13f153a3cd213eddd405444e0d0d4c9a
Alex <mntokhirjonov@gmail.com>
2025-12-06 18:56:42 +0500

    feat: frontend js, css, ejs files creation

--- src/controller/company.controller.ts
+++ src/controller/company.controller.ts
@@ -10,7 +10,7 @@ const userService = new UserService();
 companyController.goHome = (req: Request, res: Response) => {
     try {
         console.log("goHome");
-        res.send("Home Page");
+        res.render("home");
     } catch (err) {
         console.log("ERROR, goHome:", err);
     }
@@ -19,7 +19,7 @@ companyController.goHome = (req: Request, res: Response) => {
 companyController.getLogin = (req: Request, res: Response) => {
     try {
         console.log("getLogin");
-        res.send("Login Page");
+        res.render("login");
     } catch (err) {
         console.log("ERROR, getLogin:", err);
     }
@@ -28,7 +28,7 @@ companyController.getLogin = (req: Request, res: Response) => {
 companyController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup");
-        res.send("Signup Page");
+        res.render("signup");
     } catch (err) {
         console.log("ERROR, getSignup:", err);
     }
