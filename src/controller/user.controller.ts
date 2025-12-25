1295a898f8c00ab5f35e4e65d30e2a86823d6361
Alex <mntokhirjonov@gmail.com>
2025-12-25 14:10:47 +0500

    feat: develop authentication via tokens with cookies

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,8 +2,9 @@ import express, { Response, Request } from "express";
 import { T } from "../libs/types/common";
 import UserService from "../models/User.service";
 import { LoginInput, User, UserInput } from "../libs/types/user";
-import Errors from "../libs/Error";
+import Errors, { HttpCode } from "../libs/Error";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 const userService = new UserService;
 const authService = new AuthService;
@@ -15,9 +16,13 @@ userController.signup = async (req: Request, res: Response) => {
         const input: UserInput = req.body,
           result: User = await userService.signup(input),
           token = await authService.createToken(result);
-        console.log("token:", token);
         
-        res.json({member: result});
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
+        
+        res.status(HttpCode.CREATED).json({user: result, accessToken: token});
     } catch (err) {
         console.log("ERROR, signup:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
@@ -31,9 +36,13 @@ userController.login = async (req: Request, res: Response) => {
         const input: LoginInput = req.body,
           result = await userService.login(input),
           token = await authService.createToken(result);
-        console.log("token:", token);
 
-        res.json({member: result});
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
+        
+        res.status(HttpCode.CREATED).json({user: result, accessToken: token});
     } catch (err) {
         console.log("ERROR, login:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
